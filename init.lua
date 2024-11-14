c12c0f51d9d99fece6e5a6e0298c18f6acca753e
Yuzo <youzow3@yahoo.co.jp>
2024-11-14 10:39:01 +0900

    First Commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+
+vim.opt.number = true
+vim.opt.expandtab = false
+vim.opt.backup = true
+home_dir = vim.env.HOME
+vim.opt.backupdir = home_dir .. "/backup/nvim"
+
+require("config.lazy")
+
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,71 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = true
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig"
+		},
+		opts = {
+			ensure_installed = {
+				"clangd",
+				"lua_ls",
+				"mesonlsp"
+			}
+		},
+		config = true
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp"
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				expand = function(args)
+					vim.fn["vsnip#anonymous"](args.body)
+				end,
+				mapping = cmp.mapping.preset.insert({
+					['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+					['<C-v>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true })
+				}),
+				sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'buffer' }
+				})
+			})
+		end
+	},
+	{
+		"hrsh7th/vim-vsnip",
+		"hrsh7th/cmp-vsnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp"
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.clangd.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.mesonlsp.setup({})
+		end
+	}
+}
+
